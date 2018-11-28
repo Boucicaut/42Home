bc21b5fb79834e0ab84dafe098c45c0043789ab6
Benjamin VIOLLET <bviollet@e2r3p9.42.fr>
2018-11-28 20:41:38 +0100

    s

--- get_next_line.c
+++ get_next_line.c
@@ -1,61 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bviollet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/28 15:34:03 by bviollet          #+#    #+#             */
+/*   Updated: 2018/11/28 20:41:01 by bviollet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
+#include "libft/libft.h"
 
-char	*depassement(char **line, int i, int j)
+static char	*ft_error(int fd, char **line, char *str[100])
 {
-	int		diff;
-	char	*buf;
-	char	*ptr;
-
-	diff = BUFF_SIZE - j;
-	j++;
-	/* j = 1er byte post \0 */
-	if ((buf = malloc((sizeof(char) * diff))) == NULL)
+	if ((fd < 0) || (line == NULL))
 		return (NULL);
-	bzero(buf, diff);
-	ptr = memccpy(buf, line[i][j], '\n', diff);
-	return (buffer);
+	if (!str[fd])
+		if (!(str[fd] = (char *)malloc(sizeof(char) * BUFF_SIZE + 1)))
+			return (NULL);
+	return (str[fd]);
 }
-/* GET NEXT LINE WITH MALLOC */
-int	get_next_line(const int fd, char **line)
+
+static char	**ft_fillline(char **line, char *str, int *i)
 {
-	static int	i = -1;
-	int			j;
-	static char	*buf = NULL;
-
-	j = 0;
-	i++;
-	if (buf != NULL)
-	{	
-		if ((line[i] = (char*)malloc(sizeof(char) * BUFF_SIZE * 2 + 1)) == NULL)
-			return (-1);
-		memccpy(line[i], buf, '\n', 
-	}
+	*i = 0;
+	while ((str[*i]) && (str[*i] != '\n'))
+		*i = *i + 1;
+	if (*i == 0)
+		*line = ft_strdup("");
 	else
-		if ((line[i] = (char *)malloc(sizeof(char) * BUFF_SIZE + 1)) == NULL)
-			return (-1);
-	if ((read(fd, line[i], BUFF_SIZE)) > 0)
+		*line = ft_strsub(str, 0, *i);
+	return (line);
+}
+
+static char	*ft_read(const int fd, char *str)
+{
+	int			nbbytes;
+	char		buf[BUFF_SIZE + 1];
+
+	while ((nbbytes = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		while (line[i][j] != '\n')
-		{
-			if ((j + 1) == BUFF_SIZE)
-				line[i][j] = '\0';		
-			j++;
-		}
-		if (line[i][j] == '\n')
-			line[i][j] = '\0';
-
-		while (j < BUFF_SIZE)
-		{
-			i++;
-			buf = depassement(line, i, j);
-			if (memchr(buf, 10,  (BUFF_SIZE - j)) == NULL)
-		}
-
-		printf("i = %d, %s", i, line[i]);
-		return (1);	
+		buf[nbbytes] = '\0';
+		str = ft_strjoin(str, buf);
 	}
+	if (nbbytes < 0)
+		return (NULL);
+	return (str);
+}
 
-	free(line);
-	close(fd);
+int			get_next_line(const int fd, char **line)
+{
+	static char	*str[100];
+	int			i;
+
+	if ((str[fd] = ft_error(fd, line, str)) == NULL)
+		return (-1);
+	if (*str[fd])
+		*line = ft_strcpy(*line, str[fd]);
+	if ((str[fd] = ft_read(fd, str[fd])) == NULL)
+	{
+		return (-1);
+	}
+	i = 0;
+	if (str[fd][i])
+	{
+		line = ft_fillline(line, str[fd], &i);
+		if (i != 0)
+			str[fd] = ((str[fd]) + i + 1);
+		printf("Line : %s\n", *line);
+		return (1);
+	}
+	*line = ft_strdup("");
 	return (0);
 }
