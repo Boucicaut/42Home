e57bd4da46d8b2c19649090e51ba051a1b8911ac
Benjamin VIOLLET <bviollet@e2r12p1.42.fr>
2018-12-15 16:51:56 +0100

    a

--- main.c
+++ main.c
@@ -1,20 +1,89 @@
-#include "get_next_line.h"
+#include <stdio.h>
+#include "fillit.h"
 
-int	main(void)
+int	nbpieces(char **str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 1;
+	while (str[i] != NULL)
+	{
+		if (str[i][0] == '\0' && str[i + 1] != NULL)
+			j++;
+		i++;
+	}
+	return (j);
+}
+
+int	main(int argc, char **argv)
 {
 	char	**str;
-	int		k;
 	int		fd;
+	int		i;
+	char	*file;
+	t_chain	*chain;
+	t_piece	*piece;
+	t_piece	*tmp;
 
-	str = (char **)malloc(sizeof(char) * 10);
-	k = 1;
-	fd = open("LIRE", O_RDONLY);	
-	while (k == 1)
+	if ((argc != 2) && ((argc != 3) || (ft_strcmp(argv[1], "-v") != 0)))
 	{
-		k = (get_next_line(fd, str));
-		printf("'NL' \n");
-		if  (k == -1)
-			return (1);
+		ft_putstr("usage: fillit [-v : show pieces] file\n");
+		return (0);
 	}
+	if ((argc == 3) && (ft_strcmp(argv[2], "-v")))
+		file = argv[2];
+	else
+		file = argv[1];
+	if (!(fd = open(file, O_RDONLY)))
+		return (0);
+	str = input(fd);
+
+	if (error(str) == 0)
+	{
+		printf("error\n");
+		return (0);
+	}
+	else
+		printf("Valid\n");
+
+	if ((argc == 3) && (ft_strcmp(argv[1], "-v") == 0))
+		afficher(str);
+	printf("Nb pieces : %d\n", i = nbpieces(str));
+	
+	movepieces(str, 0, 0, 0);
+	afficher(str);
+
+	chain = createlist(str);
+
+	if ((argc == 3) && (ft_strcmp(argv[1], "-v") == 0))
+		afficherlinkedlist(chain);
+
+/*	 FREE LINKED LIST */
+	piece = chain->first;
+	free(chain);
+	while (piece != NULL)
+	{
+		i = 0;
+		tmp = piece;
+		piece = piece->next;
+		while (i < 4)
+		{
+			free(tmp->ptr[i]);
+			i++;
+		}
+		free(tmp->ptr);
+		free(tmp);
+	}
+
+/*	 FREE STR */
+	i = 0;
+	while (str[i] != NULL)
+		free(str[i++]);
+	free(str);
+
+	while(1);
+	close(fd);
 	return (0);
 }
