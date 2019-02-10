1155d8cda92d20cd7f0c3a5cfaa0d6a541eb0ef6
Benjamin VIOLLET <bviollet@e2r13p11.42.fr>
2019-02-10 19:58:36 +0100

    d

---
+++ Printf/srcs/pourcentb.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pourcento.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bviollet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/10 20:46:43 by bviollet          #+#    #+#             */
+/*   Updated: 2019/02/10 18:21:48 by bviollet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+
+int		pourcentb(va_list args, char *str, int *lim, int *i)
+{
+	int						j;
+	unsigned long long int	nb;
+	char					*res;
+	int						printed;
+
+	printed = 0;
+	j = *i;
+	nb = va_arg(args, unsigned long long int);
+	res = ft_convertbase(nb, 2);
+	res = checkzero(res);
+	*i = j;
+	printed = doshittythings(lim, res, 0, 'o');
+	free(lim);
+	free(res);
+	(void)str;
+	return (printed);
+}
+
+int		pourcentpourcent(va_list args, char *str, int *lim, int *i)
+{
+	int	printed;
+
+	printed = 0;
+	while (!lim[4] && lim[0]-- > 1)
+	{
+		printed++;
+		lim[2] ? ft_putchar('0') : ft_putchar(' ');
+	}
+	ft_putchar('%');
+	while (lim[4] && lim[0]-- > 1)
+	{
+		printed++;
+		lim[2] ? ft_putchar('0') : ft_putchar(' ');
+	}
+	free(lim);
+	(void)args;
+	(void)str[*i];
+	return (printed + 1);
+}
