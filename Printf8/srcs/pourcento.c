1ab4055985ce0e4533a0de3bd81896305f95c940
boucicaut <benjamin.viollet30@em-normandie.fr>
2019-02-13 13:49:56 +0100

    d

--- Printf8/srcs/pourcento.c
+++ Printf8/srcs/pourcento.c
@@ -12,6 +12,7 @@
 
 #include "../includes/ft_printf.h"
 
+#include <stdio.h>
 void	octprintall(int j, int *lim, char *res)
 {
 	int						jj;
@@ -36,25 +37,61 @@ void	octprintall(int j, int *lim, char *res)
 	}
 }
 
+int		ifoh(unsigned long long int nb, char *str, int i)
+{
+	nb = ~nb;
+	if (str[i + 1] == 'h')
+	{
+		while (nb > 128)
+			nb -= 128;
+	}
+	else
+	{
+		while (nb > 32000)
+			nb -= 32000;
+	}
+	return (nb);
+}
+
 int		pourcento(va_list args, char *str, int *lim, int *i)
 {
 	unsigned long long int	nb;
+	int						nb2;
+	long long int			nb3;
 	char					*res;
 	int						printed;
 
+	nb3 = 0;
+	nb2 = 0;
+	nb = 0;
 	printed = 0;
-	nb = va_arg(args, unsigned long long int);
+//printf("Str[*I] : %s\n", str + *i);
+	if (str[*i] == 'h')
+	{
+		nb2 = va_arg(args, int);
+//printf("Nb2 va_arg : %d\n", nb2);
+		nb = convunsigned(nb2);
+	}
+	else if (ft_issamealpha(str[*i], 'o'))
+	{
+		nb3 = va_arg(args, long long int);
+//printf("Nb3 va_arg : %lld\n", nb3);
+		nb = convunsigned(nb3);
+	}
+	else
+		nb = va_arg(args, unsigned long long int);
+//printf("Nb convsignchar : %llu\n", nb);
 	res = ft_convertbase(nb, 8);
 	if ((lim[6] || lim[0] || lim[1]) && res[0] == '0' && !res[1])
 		res[0] = '\0';
 	res = checkzero(res);
+//printf("Res : %s || Nb : %lld || ~nb : %lld\n", res, nb, ~nb);
 	while (str[*i] != '%')
 	{
 		if (str[*i] == '.')
-		{
 			lim[2] = 0;
+		if (str[*i] == '.')
 			printed = 1;
-		}
 		*i = *i - 1;
 	}
 	res[0] = printed && res[0] == '0' && !lim[6] ? '\0' : res[0];
