d0ca22cf782eec66a382da856e9064bb8a8d3c0e
boucicaut <benjamin.viollet30@em-normandie.fr>
2019-04-01 13:23:24 +0200

    Update

--- d02/ex00/another_world.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/php
-<?php
-	if ($argc < 2)
-		exit();
-	$arg = preg_replace('/[" ",\t]+/',' ',$argv[1]);
-	$arg = preg_replace('/(^[" ",\t])|([" ",\t]+$)/','',$arg);
-	echo "$arg\n";
-?>
